ced51c47b31b4184b2509a39703f1bae170ba897
Woojun <woojun0107@naver.com>
2022-10-15 18:29:42 +0900

    [Bronze III] Title: 이진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3460. 이진수/이진수.cc
+++ 백준/Bronze/3460. 이진수/이진수.cc
@@ -1,31 +1,27 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-	int t = 0;
-	cin >> t;
-	int* n = new int[t];
-	for (int i = 0; i < t; i++) cin >> n[i];
-	vector<int> biN; // 이진수로 나타낸 n
-	for (int x = 0; x < t; x++)
-	{
-		if (n[x] == 1) biN.push_back(1);
-		else
-		{
-			while (n[x] != 1)
-			{
-				biN.push_back(n[x] % 2);
-				n[x] /= 2;
-				if (n[x] == 1) biN.push_back(n[x]); // 앞에서부터 순서대로 2의0승, 2의1승, ... 순으로 정렬됨
-			}
-		}
-		for (int i = 0; i < biN.size(); i++)
-		{
-			if (biN[i] == 1) cout << i << " ";
-		}
-		biN.clear();
-		cout << "\n";
-	}
+int main () {
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+    
+    int t,n;
+    
+    cin >> t;
+    
+    for (int j = 0; j < t; j++){
+        cin >> n;
+
+        int k=0;
+
+        while(n>0){
+            if(n%2==1){
+                cout << k << " ";
+            }
+            n/=2;
+            k++;
+        }
+    }
+
+
 }
