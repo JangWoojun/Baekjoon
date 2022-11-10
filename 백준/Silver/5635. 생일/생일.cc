9ddd082dbce263305adc475d0df9f467dc594381
Woojun <woojun0107@naver.com>
2022-11-11 02:50:42 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/5635. 생일/생일.cc
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+ios::sync_with_stdio(false);
+cin.tie(NULL); cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	vector<pair<pair<int, int>, pair<int, string>>> v(n);
+	
+	for (int i = 0; i < n; i++){
+		cin >> v[i].second.second >> v[i].second.first >> v[i].first.second >> v[i].first.first;
+	}
+
+	sort(v.begin(), v.end());
+
+	cout << v[n-1].second.second << "\n" << v[0].second.second << "\n";
+    
+}
+
+
