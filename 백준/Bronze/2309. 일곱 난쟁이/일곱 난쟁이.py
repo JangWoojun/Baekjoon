940ee35984a56e158c40699594d2eed2dbe8493f
Woojun <woojun0107@naver.com>
2023-02-04 08:09:37 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -2,19 +2,10 @@ import sys
 from itertools import combinations
 input = sys.stdin.readline
 
-a = []
-ans = []
-
-for _ in range(9):
-    a.append(int(input()))
+a = [int(input()) for _ in range(9)]
     
 for i in combinations(a, 7):
-    if i[0] + i[1] + i[2] + i[3] + i[4] + i[5] + i[6] == 100:
-        for j in range(7):
-            ans.append(i[j])
+    if sum(i) == 100:
+        for j in sorted(i):
+            print(j)
         break
-
-ans.sort()
-
-for l in ans:
-    print(l)
