e28e614af38aa4223f01bd2fb9d33f6284ec845e
Woojun <woojun0107@naver.com>
2023-02-04 08:36:36 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -6,14 +6,8 @@ n, k = map(int, input().split())
 a = [int(input()) for _ in range(n)]
 a.reverse()
 
-i = 0
-
-while k!=0:
-    if k - a[i] < 0:
-        i+=1
-    else:
-        k-=a[i]
-        count+=1
-
-print(count)
+for i in a:
+    count += k//i
+    k%=i
     
+print(count)
