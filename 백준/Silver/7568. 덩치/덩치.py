1b59028a1985ed18e3fdf6e18176850aeb6e3aba
SUS <woojun0107@naver.com>
2023-03-12 12:55:24 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,20 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+ 
+arr = []  
+ans = []  
+for i in range(n):
+    a, b = map(int, input().split())
+    arr.append((a, b))  
+ 
+for i in range(n):
+    count = 0
+    for j in range(n):
+        if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:  
+            count += 1 
+    ans.append(count + 1)  
+ 
+for d in ans:
+    print(d,end=" ")
