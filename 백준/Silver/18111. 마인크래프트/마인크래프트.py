3aeaa0687092c1670609379eacef86171db2f7d4
SUS <woojun0107@naver.com>
2023-03-25 22:51:41 +0900

    [Silver II] Title: 마인크래프트, Time: 804 ms, Memory: 117424 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,33 +1,27 @@
 import sys
+input = sys.stdin.readline
 
-n, m, b = map(int, sys.stdin.readline().split())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-answer = sys.maxsize
-idx = 0
+a, b, c = map(int, input().split())
 
-# 0층부터 256층까지 반복
-for target in range(257):
-    max_target, min_target = 0, 0
+arr = [list(map(int, input().split())) for _ in range(a)]
+chk1 = sys.maxsize
+chk2 = 0
+for i in range(257):
+    st = 0
+    re = 0
 
-    # 반복문을 통해 블록을 확인
-    for i in range(n):
-        for j in range(m):
-
-            # 블록이 층수보다 더 크면
-            if graph[i][j] >= target:
-                max_target += graph[i][j] - target
-
-            # 블록이 층수보다 더 작으면
+    for j in range(a):
+        for l in range(b):
+            if arr[j][l] >= i:
+                re+=arr[j][l] - i
             else:
-                min_target += target - graph[i][j]
-
-    # 블록을 뺀 것과 원래 있던 블록의 합과 블록을 더한 값을 비교
-    # 블록을 뺀 것과 원래 있던 블록의 합이 더 커야 층을 만들 수 있음.
-    if max_target + b >= min_target:
-        # 시간 초를 구하고 최저 시간과 비교 
-        if min_target + (max_target * 2) <= answer:
-        	# 0부터 256층까지 비교하므로 업데이트 될수록 고층의 최저시간
-            answer = min_target + (max_target * 2) # 최저 시간
-            idx = target # 층수
-
-print(answer, idx)
+                st+=i-arr[j][l]
+            
+    if re+c < st:
+        continue
+    
+    if chk1 >= re*2 + st:
+        chk1 = re*2 + st
+        chk2 = i
+        
+print(chk1, chk2)
