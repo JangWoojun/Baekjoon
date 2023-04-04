7a6f2289be25b860c368a9e0afafb6732481bacb
SUS <woojun0107@naver.com>
2023-04-04 19:51:11 +0900

    [Silver V] Title: 분수찾기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,15 @@
+from sys import stdin
+input = stdin.readline
+
+n = int(input())
+
+i = 1
+
+while n>i:
+    n-=i
+    i+=1
+    
+if i%2 == 1:
+    print(f"{i+1-n}/{n}")
+else:
+    print(f"{n}/{i+1-n}")
