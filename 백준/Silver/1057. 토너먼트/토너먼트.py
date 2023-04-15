5ddbaf6b534b0a8f191cb232e8e688ffc3bd6a88
SUS <woojun0107@naver.com>
2023-04-15 15:32:37 +0900

    [Silver IV] Title: 토너먼트, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1057. 토너먼트/토너먼트.py
@@ -0,0 +1,12 @@
+from sys import stdin
+input = stdin.readline
+
+_, a, b = map(int, input().split())
+count = 0
+
+while a != b:
+    a -= (a // 2)
+    b -= (b // 2)
+    count += 1
+    
+print(count)
