ffeae90a44334ffb44abf9258c75d5b07c34e7c7
SUS <woojun0107@naver.com>
2023-04-19 21:08:46 +0900

    [Silver III] Title: 주유소, Time: 100 ms, Memory: 41400 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,9 +1,10 @@
 from sys import stdin
+from collections import deque
 input = stdin.readline
 
 n = int(input())
 
-arr1 = list(map(int, input().split()))
+arr1 = deque(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 arr2.pop()
 
@@ -11,12 +12,10 @@ count = 0
 
 for i in range(n-1):
     if arr2[i] == min(arr2):
-        a = 0
-        for j in range(i, n-1):
-            a+=arr1[j]
-        count+=(a * arr2[i])
+        count+=(sum(arr1) * arr2[i])
         break
     else:
         count+= arr2[i] * arr1[i]
+        arr1.popleft()
 
 print(count)
