5198fdf2ddc19c1cb07768eb7281e8531ada1a63
SUS <woojun0107@naver.com>
2023-12-27 14:31:52 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.cc
@@ -6,10 +6,7 @@ vector<int>v(26, 0);
 int main() {
     cin >> s;
 
-    for (int i = 0; i < s.length(); i++) {
-        v[(int)s[i] - 97]+=1;
-    }
-    
+    for(char c : s) v[c - 'a']++;
     for(auto it: v) cout << it << " ";
     
     return 0;
