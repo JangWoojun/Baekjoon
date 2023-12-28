a61a42e51c84a8c3e5a06bcbb623a6d48aeb1bea
SUS <woojun0107@naver.com>
2023-12-28 09:56:38 +0900

    [Bronze I] Title: ROT13, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/11655. ROT13/ROT13.cc
+++ 백준/Bronze/11655. ROT13/ROT13.cc
@@ -1,30 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+string s;
 
-int main () {
-    string s;
-
-    getline(cin,s);
-
-    for (size_t i = 0; i < s.length(); i++){
-        if (int(s[i])>47 and 58 > int(s[i])){
-            cout << char(int(s[i]));
-        }
-        else if (int(s[i])!=32){
-            if (int(s[i])>77 and int(s[i])<91){
-                cout << char(int(s[i])-13);
-            }
-            else if (int(s[i])>109){
-                cout << char(int(s[i])-13);
-            }
-            else {
-                cout << char(int(s[i])+13);
-            }
-            
-        }
-        else {
-            cout << " ";
+int main() {
+    getline(cin, s);
+    
+    for(auto it: s) {
+        if(it < 65 || 122 < it) cout << it;
+        
+        if(it > 64 && it < 91) {
+            if(it+13 > 90) cout << char(it+13-26);
+            else cout << char(it+13);
+        } else if(it > 96 && it < 123) {
+            if(it+13 > 122) cout << char(it+13-26);
+            else cout << char(it+13);
         }
     }
+    
 
+    return 0;
 }
